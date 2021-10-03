b5f5f2accab915c13f4bfc8d3eeadfa50daa3cd2
victor castro <victor_castro_01@hotmail.com>
2021-10-03 16:12:14 -0400

    isPalidrome completed

--- index.js
+++ index.js
@@ -1,9 +1,39 @@
+//Write a function that takes a string as an argument
+//if the string is a Palidrome then return true, otherwise return false
+
 function isPalindrome(word) {
   // Write your algorithm here
+ 
+  let wordForward = [];
+  let wordBackward = []; 
+ 
+  for(let letter of word){
+    wordForward.push(letter);
+    wordBackward.unshift(letter)
+  }
+  
+  return compareArrays(wordForward,wordBackward)
 }
 
+function compareArrays(arr1,arr2){
+    for(let i =0; i<arr1.length;i++){
+      if(arr1[i]===arr2[i]){
+        return true
+      }else{
+        return false
+      }
+    }
+}
+
+
+
 /* 
-  Add your pseudocode here
+  //Create two arrays to store each letter of the input word, one for normal spelling and one for backward spelling
+  //Create two empty arrays to store both spelling (normak and backward)
+  //Create a loop to iterate over the input word and push them to the arrays
+  //create a separate function that takes two arrays as arguments and compare their elements
+  //run the new function at the end of the main function (isPalidrome) to compare the two arrays that we created 
+
 */
 
 /*
